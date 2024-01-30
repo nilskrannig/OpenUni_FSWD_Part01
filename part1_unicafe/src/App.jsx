162cbe63d016504752a8b882bf19de06d014eb45
nilskrannig <nils.krannig@gmx.de>
2024-01-30 12:10:32 +0100

    Part1_Exercises unicafe: Step 3

--- part1_unicafe/src/App.jsx
+++ part1_unicafe/src/App.jsx
@@ -1,14 +1,37 @@
 import { useState } from 'react'
 
-const Title = ({title}) => <h2>{title}</h2>;
+const Title = ({ title }) => <h2>{title}</h2>;
 
-const Button = ({onClick, buttonText}) => {
+const Button = ({ onClick, buttonText }) => {
   return <button onClick={onClick}>{buttonText}</button>
 };
 
-const DisplayValue = ({name, value}) => {
+const DisplayValue = ({ name, value }) => {
   return <div>
     {name} {value}
+  </div>
+}
+
+const Statistics = ({good, neutral, bad}) => {
+  function getPositive() {
+    return (good / getTotalNumberOfVotes()) * 100;
+  }
+
+  function getAverage() {
+    return (good * 1 + neutral * 0 + bad * -1) / getTotalNumberOfVotes();
+  }
+
+  function getTotalNumberOfVotes() {
+    return good + neutral + bad;
+  }
+
+  return <div>
+    <DisplayValue name='good' value={good} />
+    <DisplayValue name='neutral' value={neutral} />
+    <DisplayValue name='bad' value={bad} />
+    <DisplayValue name='all' value={getTotalNumberOfVotes()} />
+    <DisplayValue name='average' value={getAverage()} />
+    <DisplayValue name='positive' value={getPositive() + ' %'} />
   </div>
 }
 
@@ -40,26 +63,9 @@ function App() {
       <Button onClick={handleNeutral} buttonText='neutral' />
       <Button onClick={handleBad} buttonText='bad' />
       <Title title='statistics' />
-      <DisplayValue name='good' value={good} />
-      <DisplayValue name='neutral' value={neutral} />
-      <DisplayValue name='bad' value={bad} />
-      <DisplayValue name='all' value={getTotalNumberOfVotes()}/>
-      <DisplayValue name='average' value={getAverage()}/>
-      <DisplayValue name='positive' value={getPositive() + ' %'}/>
+      <Statistics good={good} neutral={neutral} bad={bad}/>
     </div>
   )
-
-  function getPositive() {
-    return (good / getTotalNumberOfVotes()) * 100;
-  }
-
-  function getAverage() {
-    return (good * 1 + neutral * 0 + bad * -1) / getTotalNumberOfVotes();
-  }
-
-  function getTotalNumberOfVotes() {
-    return good + neutral + bad;
-  }
 }
 
 export default App
